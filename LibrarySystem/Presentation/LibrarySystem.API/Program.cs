a8d76e2d4fee797f678610fcba0abdb48e5329ca
Subhan <subhanshh@code.edu.az>
2026-07-31 03:51:30 +0400

    fix: the issues have been resolved

--- LibrarySystem/Presentation/LibrarySystem.API/Program.cs
+++ LibrarySystem/Presentation/LibrarySystem.API/Program.cs
@@ -12,7 +12,7 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
         builder.Services
             .AddApplicationServices()
-            .AddInfrastructureServices()
+            .AddInfrastructureServices(builder.Configuration)
             .AddPersistenceServices(builder.Configuration);
 
         builder.Services.AddControllers();
