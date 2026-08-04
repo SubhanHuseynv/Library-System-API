acf8645f28cd2c0a5fab3b69acf609302ce14563
Subhan <subhanshh@code.edu.az>
2026-08-04 23:20:45 +0400

    feat: add automatic audit timestamps

--- LibrarySystem/src/Infrastructure/LibrarySystem.Persistence/Context/AppDbContext.cs
+++ LibrarySystem/src/Infrastructure/LibrarySystem.Persistence/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using LibrarySystem.Domain.Entities;
+using LibrarySystem.Domain.Entities.common;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
@@ -16,8 +17,28 @@ public class AppDbContext : IdentityDbContext<AppUser>
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         base.OnModelCreating(modelBuilder);
     }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseAccountable>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+                    break;
+            }
+        }
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
     public DbSet<Book> Books { get; set; }
     public DbSet<Author> Authors { get; set; }
     public DbSet<Member> Members { get; set; }
     public DbSet<BookMember> BookMembers { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<BookCategory> BookCategories { get; set; }
 }
