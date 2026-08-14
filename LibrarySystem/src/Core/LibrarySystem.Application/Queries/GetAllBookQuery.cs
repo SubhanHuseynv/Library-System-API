90094f9baef22af77ec1a4343fc79c2f64a948f7
Subhan <subhanshh@code.edu.az>
2026-08-14 18:37:30 +0400

    feat: add query logic

---
+++ LibrarySystem/src/Core/LibrarySystem.Application/Queries/GetAllBookQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace LibrarySystem.Application.Queries;
+
+public class GetAllBookQuery
+{
+    public string? Filter { get; set; }
+    public int ConSort { get; set; }
+    public int Page { get; set; }
+    public int Take { get; set; }
+    public int MinPrice { get; set; }
+    public int MaxPrice { get; set; }
+}
--- LibrarySystem/src/Core/LibrarySystem.Application/Queries/GetAllOrderQuery.cs
+++ LibrarySystem/src/Core/LibrarySystem.Application/Queries/GetAllOrderQuery.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LibrarySystem.Application.Queries;
 
-namespace LibrarySystem.Application.Queries
+public class GetAllOrderQuery
 {
-    public class GetAllOrderQuery
-    {
-        public long? CustomerId { get; set; }
-        public DateTime? CreatedAt { get; set; }
+    public long CustomerId { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
 
-    }
 }
